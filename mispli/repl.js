7a21791c40598cab72a7e6219017f13bf069d1e7
mooz <stillpedant@gmail.com>
2010-04-18 13:00:58 +0900

    More intaractively

--- mispli/repl.js
+++ mispli/repl.js
@@ -39,7 +39,7 @@
 
          var input = ev.target;
 
-         if (!input.value || Mispli.syntaxChecker(input.value))
+         if (!input.value || input.value[0] === "\\" || Mispli.syntaxChecker(input.value))
              input.style.backgroundColor = replInputOriginalBG;
          else
              input.style.backgroundColor = replInputWargingBG;
@@ -75,17 +75,34 @@
          return elem;
      }
 
-     window.print = function (txt) {
+     Mispli.print = function (txt) {
          echo(txt, REPL_OUTPUT);
      };
 
      function getScrollMaxY() { return window.scrollMaxY || document.documentElement.scrollHeight - document.documentElement.clientHeight; }
 
-     function replEval(input) {
-         var scrollFrom = window.scrollY;
-         var inputCode  = input.value;
+     function replInsertInputsAndResults(inputCode, blocks) {
+         var scrollFrom;
 
-         echo(inputCode, REPL_INPUT);
+         function gradPrinter (i) {
+             if (i >= blocks.length)
+                 return;
+
+             var savedY = window.scrollY;
+
+             echo(Mispli.sexpToStr(blocks[i]), REPL_INPUT);
+             echo(Mispli.sexpToStr(Mispli.evalBlock(blocks[i])), REPL_RESULT);
+
+             smoothScrollY(savedY, getScrollMaxY(), function () {
+                               window.setTimeout(function () { gradPrinter(i + 1); }, 500);
+                           });
+         }
+
+         gradPrinter(0);
+     }
+
+     function replEval(input) {
+         var inputCode  = input.value;
 
          if (inputCode[0] === "\\")
          {
@@ -93,17 +110,17 @@
                  "?" : ["Display this help", function () {
                             echo("COMMANDS", REPL_RESULT);
                             for (var k in commands)
-                                print("\\" + k + "\t" + commands[k][0]);
+                                Mispli.print("\\" + k + "\t" + commands[k][0]);
                         }],
-                 "b" : ["Show builtin functions", function () { for (var k in Mispli.builtins) print(k); }],
-                 "m" : ["Show macros", function () { for (var k in Mispli.macros) print(k); }],
-                 "s" : ["Show special forms", function () { for (var k in Mispli.specials) print(k); }],
+                 "b" : ["Show builtin functions", function () { for (var k in Mispli.builtins) Mispli.print(k); }],
+                 "m" : ["Show macros", function () { for (var k in Mispli.macros) Mispli.print(k); }],
+                 "s" : ["Show special forms", function () { for (var k in Mispli.specials) Mispli.print(k); }],
                  "f" : ["Show global functions", function () {
                             for (var k in Mispli.globalEnv)
                             {
                                 var sym = Mispli.globalEnv[k];
                                 if (Mispli.hasSymbolType(sym, Mispli.SYM_FUNCTION))
-                                    print(sym.name + "\t" + Mispli.sexpToStr(Mispli.getSymbolValue(sym, Mispli.SYM_FUNCTION)));
+                                    Mispli.print(sym.name + "\t" + Mispli.sexpToStr(Mispli.getSymbolValue(sym, Mispli.SYM_FUNCTION)));
                             }
                         }],
                  "v" : ["Show global variables", function () {
@@ -111,7 +128,7 @@
                             {
                                 var sym = Mispli.globalEnv[k];
                                 if (Mispli.hasSymbolType(sym, Mispli.SYM_VARIABLE))
-                                    print(sym.name + "\t" + tos.sexpToStr(Mispli.getSymbolValue(sym, Mispli.SYM_VARIABLE)));
+                                    Mispli.print(sym.name + "\t" + tos.sexpToStr(Mispli.getSymbolValue(sym, Mispli.SYM_VARIABLE)));
                             }
                         }],
                  "j" : ["Evaluate JavaScript Code", function (command) {
@@ -126,20 +143,27 @@
              };
 
              function unknown(cmd) {
-                 print("Unknown command `" + cmd + "'. Type \\? to see available commands");
+                 Mispli.print("Unknown command `" + cmd + "'. Type \\? to see available commands");
              }
 
+             echo(inputCode, REPL_INPUT);
+
              (commands[inputCode[1]] || { "1" : unknown })[1](inputCode);
+
+             var savedY = window.scrollY;
+             smoothScrollY(savedY, getScrollMaxY());
          }
          else
          {
-             try {
-                 Mispli.evalLisp(inputCode)
-                     .map(function (result) { return Mispli.sexpToStr(result); })
-                     .forEach(function (str) { echo(str, REPL_RESULT); });
-             } catch (x) {
+             try
+             {
+                 var blocks  = Mispli.parse(inputCode, true);
+                 replInsertInputsAndResults(inputCode, blocks);
+             }
+             catch (x)
+             {
                  if (x.stack)
-                     echo("js error ::\n" + x + "\n" + x.stack, "repl-error", REPL_ERROR);
+                     echo("js error:\n" + x + "\n" + x.stack, "repl-error", REPL_ERROR);
                  else
                      echo(x, "repl-error", REPL_ERROR);
              }
@@ -150,9 +174,6 @@
          histIndex = 0;
 
          input.value = "";
-
-         // window.scrollTo(window.scrollX, scrollFrom);
-         smoothScrollY(scrollFrom, getScrollMaxY());
      }
 
      function echo(text, className) {
@@ -167,31 +188,42 @@
      var replScrollTimer = null;
 
      // http://piro.sakura.ne.jp/latest/blosxom/mozilla/xul/2009-04-08_tween.htm
-     function smoothScrollY(from, to) {
-         var delta     = to - from;
+     function smoothScrollY(from, to, callback) {
+         if (window.scrollY !== from)
+             window.scrollTo(from, window.scrollX);
 
-         // if (delta < 200)
-         // {
-         //     window.scrollTo(window.scrollX, to);
-         //     return;
-         // }
+         var delta = to - from;
+
+         if (delta < 20)
+         {
+             window.scrollTo(to, window.scrollX);
+             if (typeof callback === "function")
+                 callback();
+             return;
+         }
 
          var duration  = Math.sqrt(delta / 50) * 200;
-         var startTime = Date.now();
+         var startTime = +new Date();
 
          if (replScrollTimer)
              window.clearInterval(replScrollTimer), replScrollTimer = null;
 
          replScrollTimer = window.setInterval(
              function () {
-                 var progress = Math.min(1, (Date.now() - startTime) / duration);
+                 var progress = Math.min(1, (+new Date() - startTime) / duration);
                  var x = window.scrollX;
                  var y = (progress === 1) ? to : from + (delta * progress);
 
                  window.scrollTo(x, y);
+
                  if (progress === 1)
+                 {
                      window.clearInterval(replScrollTimer);
-             }, 32);
+
+                     if (typeof callback === "function")
+                         callback();
+                 }
+             }, 16);
      }
 
      // ============================================================ //
@@ -245,9 +277,10 @@
      function setUpSnippetsArea() {
          var ul = $("main-repl-snippets");
 
-         [["Fibonatti", "(defun fib (n) (cond ((= n 0) 0) ((= n 1) 1) ((= n 2) 1) (t (+ (fib (1- n)) (fib (- n 2)))))) (fib 10)"],
-          ["FizzBuzz", '(mapcar (lambda (x) (cond ((= (% x 15) 0) "FizzBuzz") ((= (% x 5) 0) "Buzz") ((= (% x 3) 0) "Fizz") (t x))) (iota 100 1))'],
-          ["Lexical-Closure-Test", "(defun gen-counter (n) (lambda (&optional d) (setq n (+ n (or d 1))))) (setq counter (gen-counter 10)) (funcall counter) (funcall counter 10) (funcall counter 40) (funcall counter -20)"]
+         [["Fibonatti", "(defun fib (n) (cond ((= n 0) 0) ((= n 1) 1) ((= n 2) 1) (t (+ (fib (1- n)) (fib (- n 2)))))) (fib 3) (fib 5) (fib 10)"],
+          ["FizzBuzz", '(mapc \'print (mapcar (lambda (x) (cond ((= (% x 15) 0) "FizzBuzz") ((= (% x 5) 0) "Buzz") ((= (% x 3) 0) "Fizz") (t x))) (iota 100 1)))'],
+          ["Lexical-Closure-Test", "(defun gen-counter (n) (lambda (&optional d) (setq n (+ n (or d 1))))) (setq counter (gen-counter 10)) (funcall counter) (funcall counter 10) (funcall counter 40) (funcall counter -20)"],
+          ["Tak (very slow)", "(defun tak (x y z) (if (<= x y) y (tak (tak (1- x) y z) (tak (1- y) z x) (tak (1- z) x y)))) (time (tak 8 4 0))"]
          ].forEach(function (row) {
                        ul.appendChild(createSnippet(row[0], row[1]));
                    });
@@ -258,10 +291,10 @@
          replInputOriginalBG = input.style.backgroundColor;
          input.addEventListener("keyup", inputBoxKeyHandler, false);
 
-         print("MiSPLi 0.1.3");
-         print("Input Lisp code and press `Enter' key.");
-         print("You can take back history by pressing `Up' and `Down' key.");
-         print("Type \\? to see available REPL commands.");
+         Mispli.print("MiSPLi 0.1.3");
+         Mispli.print("Input Lisp code and press `Enter' key.");
+         Mispli.print("You can take back history by pressing `Up' and `Down' key.");
+         Mispli.print("Type \\? to see available REPL commands.");
 
          setUpHelperArea();
          setUpSnippetsArea();
